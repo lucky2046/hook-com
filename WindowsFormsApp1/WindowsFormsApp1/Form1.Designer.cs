a0555e4a81b10db97780baa6da9247a76b1bf808
luckytest <11626708+luckywuhan@users.noreply.github.com>
2020-12-11 18:39:36 +0800

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.axSetTipOcx1 = new AxSetTipOcxLib.AxSetTipOcx();
+            ((System.ComponentModel.ISupportInitialize)(this.axSetTipOcx1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(901, 398);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axSetTipOcx1
+            // 
+            this.axSetTipOcx1.Enabled = true;
+            this.axSetTipOcx1.Location = new System.Drawing.Point(375, 179);
+            this.axSetTipOcx1.Name = "axSetTipOcx1";
+            this.axSetTipOcx1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axSetTipOcx1.OcxState")));
+            this.axSetTipOcx1.Size = new System.Drawing.Size(100, 50);
+            this.axSetTipOcx1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Controls.Add(this.axSetTipOcx1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axSetTipOcx1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private AxSetTipOcxLib.AxSetTipOcx axSetTipOcx1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyHook;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using TestDispatchUtility;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            HookDemo();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //long ret = axSetTipOcx1.add(1, 2);
+            //Console.WriteLine(ret.ToString());
+            axSetTipOcx1.show("1");
+        }
+
+        //https://docs.microsoft.com/zh-CN/dotnet/api/system.runtime.interopservices._attribute.getidsofnames?view=netframework-4.8#System_Runtime_InteropServices__Attribute_GetIDsOfNames_System_Guid__System_IntPtr_System_UInt32_System_UInt32_System_IntPtr_
+        //public void GetIDsOfNames(ref Guid riid, IntPtr rgszNames, uint cNames, uint lcid, IntPtr rgDispId);
+
+        delegate void InvokeDelegate(uint dispIdMember, ref System.Guid riid, uint lcid, short wFlags, System.IntPtr pDispParams, System.IntPtr pVarResult, System.IntPtr pExcepInfo, System.IntPtr puArgErr);
+        delegate void GetIDsOfNamesDelegate(ref System.Guid riid, System.IntPtr rgszNames, uint cNames, uint lcid, System.IntPtr rgDispId);
+
+        private void InvokeHook(uint dispIdMember, ref System.Guid riid, uint lcid, short wFlags, System.IntPtr pDispParams, System.IntPtr pVarResult, System.IntPtr pExcepInfo, System.IntPtr puArgErr)
+        {
+            string uuid = riid.ToString();
+            MessageBox.Show("Invoke hooked");
+            //return IntPtr.Zero;
+        }
+
+        private void GetIDsOfNamesHook(ref System.Guid riid, System.IntPtr rgszNames, uint cNames, uint lcid, System.IntPtr rgDispId)
+        {
+            string uuid = riid.ToString();
+            MessageBox.Show("GetIDsOfNames hooked");
+        }
+
+        public delegate Int32 AddDelegate(Int32 a1, Int32 a2);
+        public Int32 AddHooked(Int32 a1, Int32 a2)
+        {
+            MessageBox.Show("AddHooked");
+            return a1 + a2 + 100;
+        }
+
+        public delegate void ShowDelegate(string strText);
+        public void ShowHooked([MarshalAs(UnmanagedType.LPWStr)] string strText)
+        {
+            MessageBox.Show("ShowHooked");
+        }
+
+        private void HookDemo()
+        {
+            try
+            {
+                string clsid = "93BA3E69-5E03-4AEA-AC5B-99FC4E02B12F";
+                string iid = "6C149F97-4F38-437D-8EE3-8734302A6F99";
+                var cci = new EasyHook.COMClassInfo(new Guid(clsid), new Guid(iid), 2);
+                cci.Query();
+                bool isloaded = cci.IsModuleLoaded();                               
+                IntPtr methodPointer = cci.MethodPointers[0];//QueryInterface - 0 AddRef - 1 Release - 2
+                var hook = EasyHook.LocalHook.Create(
+                            cci.MethodPointers[0],
+                            new ShowDelegate(ShowHooked),
+                            this
+                            );
+                hook.ThreadACL.SetExclusiveACL(new int[] { });
+
+                //Original = (IDispatchImplType)(object)Marshal.GetDelegateForFunctionPointer(cci.MethodPointers[0], typeof(T));
+                // call com interface
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }  
+        }
+    }
+}
